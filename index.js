5ffd3cb22bfa6ceca336f860cb66d93740275da7
shloku8 <50832393+shloku8@users.noreply.github.com>
2019-05-26 13:41:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+console.log("Hello World");
+var arr=[10,20,30,40,50];
+var n1;
+for(n1 in arr)
+{
+    console.log(arr[n1]);
+}
+var i;
+i=10;
+while(i>0)
+{
+	console.log(i);
+}
+i=10;
+do
+{
+	console.log(i);
+}while(i>10);
+var e;
+e=2;
+switch(e)
+{
+	case 1:console.log("Hello");
+	break;
+	case 2:console.log("World");
+	break;
+	case 3:console.log("Earth");
+	break;
+	default:console.log("Default");
+}
+function factorial(num)
+{
+	if(num==0)
+      {
+      	return(0);
+      }
+    if(num==1)
+    {
+    	return(1);
+    }
+    while(num!=0)
+    { 
+	return(num*num-1);
+	num--;
+	} 
+}
